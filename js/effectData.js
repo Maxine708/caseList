28d0bfee830f0154344285fcd61aeca2a1c66ecb
youbinbin <youbinbin@192.168.184.202:29418>
2018-09-07 10:17:00 +0800

    1

---
+++ js/effectData.js
@@ -0,0 +1,310 @@
+var eData = {
+	sorts : ['专题案例','交互效果','小游戏','支付宝案例'],
+	alltags : [
+		['','PC','H5','抽奖','答题','VR全景','视频'],
+		['','PC','H5','抽奖'],
+		['','H5','抽奖','重力感应','抓娃娃'],
+		['','支付宝项目案例']
+	],
+	data : [
+		{	
+			id: "77",
+			title: "坤音四子福利大解锁，玩游戏现在就开抢！",
+			platform: "H5",
+			sort: "小游戏",
+			cover: "imgs/zt/20180814/baiquelin/game1.jpg",
+			demourl: "http://www1.pclady.com.cn/zt/sh20180814/h5/baiquelin/index.html", 
+			apiurl: "", 
+			keywords: "小游戏,canvas",
+			desc: {
+				intro: "H5内置四款小游戏，跳黄包车小游戏，接宝物小游戏，接娃娃小游戏。html5+canvas小游戏", 
+				compatible: "" 
+			}
+		},
+		{
+			id: "77",
+			title: "benir清素超级奶爸大挑战",
+			platform: "H5",
+			sort: "小游戏",
+			cover: "imgs/zt/20180814/baiquelin/game2.jpg",
+			demourl: "http://www1.pcbaby.com.cn/zt/sh20160603/qingsu/index.html", 
+			apiurl: "", 
+			keywords: "拖拽小游戏,canvas",
+			desc: {
+				intro: "H5页面包含拖拽小游戏+摇一摇效果实现", 
+				compatible: "" 
+			}
+		},
+		{
+			id: "77",
+			title: "老虎机转动抽奖",
+			platform: "pc",
+			sort: "小游戏",
+			cover: "imgs/zt/2018/pc/cover5.png",
+			demourl: "http://www1.pcauto.com.cn/zt/sh20170829/xzmw/index.html", 
+			apiurl: "", 
+			keywords: "抽奖",
+			desc: {
+				intro: "pc老虎机游戏抽奖效果", 
+				compatible: "" 
+			}
+		},
+		
+		{
+		  id: "1",
+		  title: "探索魂动红-CSS3D实现vr全景",
+		  landscape : false,
+		  platform: "H5,VR全景",
+		  sort: "专题案例",
+		  cover: "imgs/zt/20180508/maxda/cover.jpg",
+		  images: [],
+		  demourl: "http://www1.pcauto.com.cn/zt/sh20180508/maxda/index.html",
+		  apiurl: "",
+		  keywords: "重力感应",
+		  desc: {
+		    intro: "H5通过css3d实现的vr全景效果，在手机端支持触摸划屏旋转场景，并支持手机重力感应"
+		    // compatible: "pc端仅高版本有动画效果，IE低版本为静态效果"
+		  }
+			// usedTime : '设计3天&emsp;前端1.5天&emsp;详询设计和前端负责人'
+		},
+		{
+		  id: "1",
+		  title: "支付宝拆红包和领取优惠券专题活动",
+		  landscape : false,
+		  platform: "支付宝项目案例",
+		  sort: "支付宝案例",
+		  cover: "imgs/zt/20180829/siwei/cover.jpg",
+		  images: [],
+		  demourl: "https://www.jianshu.com/p/97277ec728ec",
+		  apiurl: "",
+		  keywords: "支付宝",
+		  desc: {
+		    intro: "通用类各种汽车品牌支付宝红包项目（点击进入博客分析代码）"
+		    // compatible: "pc端仅高版本有动画效果，IE低版本为静态效果"
+		  }
+			// usedTime : '设计3天&emsp;前端1.5天&emsp;详询设计和前端负责人'
+		},
+		{
+		  id: "1",
+		  title: "好友PK答题",
+		  landscape : false,
+		  platform: "H5,答题",
+		  sort: "专题案例",
+		  cover: "imgs/zt/2018/beiyinmei/cover.jpg",
+		  images: [],
+		  demourl: "https://www.jianshu.com/p/e3f329c70cb4",
+		  apiurl: "",
+		  keywords: "机器人答题,邀请好友答题",
+		  desc: {
+		    intro: "H5邀请好友PK答题,项目已下线,点击进入博客解析页面"
+		    // compatible: "pc端仅高版本有动画效果，IE低版本为静态效果"
+		  }
+			// usedTime : '设计3天&emsp;前端1.5天&emsp;详询设计和前端负责人'
+		},
+		{
+		  id: "1",
+		  title: "转盘抽奖",
+		  landscape : false,
+		  platform: "PC,抽奖",
+		  sort: "专题案例",
+		  cover: "imgs/zt/2018/zhuanpan/cover.jpg",
+		  images: [],
+		  demourl: "http://www1.pcauto.com.cn/zt/sh180531/geely/index.html",
+		  apiurl: "",
+		  keywords: "转盘抽奖",
+		  desc: {
+		    intro: "PC转盘抽奖"
+		    // compatible: "pc端仅高版本有动画效果，IE低版本为静态效果"
+		  }
+			// usedTime : '设计3天&emsp;前端1.5天&emsp;详询设计和前端负责人'
+		},
+		{
+		  id: "1",
+		  title: "Ajax请求和倒计时功能",
+		  landscape : false,
+		  platform: "PC",
+		  sort: "专题案例",
+		  cover: "imgs/zt/2018/ajax/cover.png",
+		  images: [],
+		  demourl: "http://www1.pcauto.com.cn/zt/sh180110/ford/index.html",
+		  apiurl: "",
+		  keywords: "Ajax请求,倒计时",
+		  desc: {
+		    intro: "PC页面Ajax请求兼容主流浏览器"
+		    // compatible: "pc端仅高版本有动画效果，IE低版本为静态效果"
+		  }
+			// usedTime : '设计3天&emsp;前端1.5天&emsp;详询设计和前端负责人'
+		},
+		{
+		  id: "1",
+		  title: "echart图表展示",
+		  landscape : false,
+		  platform: "PC,抽奖",
+		  sort: "专题案例",
+		  cover: "imgs/zt/2018/pc/cover1.jpg",
+		  images: [],
+		  demourl: "http://www1.pconline.com.cn/zt/sh20170215/gncz/index.html",
+		  apiurl: "",
+		  keywords: "echart图表",
+		  desc: {
+		    intro: "提交数据，接口返回数据动态生成echart图表。（接口已下线，仅供参考代码）"
+		    // compatible: "pc端仅高版本有动画效果，IE低版本为静态效果"
+		  }
+			// usedTime : '设计3天&emsp;前端1.5天&emsp;详询设计和前端负责人'
+		},
+		{
+		  id: "1",
+		  title: "svg全国地图展示",
+		  landscape : false,
+		  platform: "PC,抽奖",
+		  sort: "专题案例",
+		  cover: "imgs/zt/2018/pc/cover4.png",
+		  images: [],
+		  demourl: "http://www1.pchouse.com.cn/zt/sh170420/bull/index.html",
+		  apiurl: "",
+		  keywords: "svg地图展示",
+		  desc: {
+		    intro: "svg全国地图展示，点击呈现浮窗跳转"
+		    // compatible: "pc端仅高版本有动画效果，IE低版本为静态效果"
+		  }
+			// usedTime : '设计3天&emsp;前端1.5天&emsp;详询设计和前端负责人'
+		},
+		{
+		  id: "1",
+		  title: "模仿朋友圈h5全屏视频",
+		  landscape : false,
+		  platform: "h5,视频",
+		  sort: "专题案例",
+		  cover: "imgs/zt/2018/h5/cover3.png",
+		  images: [],
+		  demourl: "https://www.jianshu.com/p/2db8b33a2800",
+		  apiurl: "",
+		  keywords: "svg地图展示",
+		  desc: {
+		    intro: "svg全国地图展示，点击呈现浮窗跳转"
+		    // compatible: "pc端仅高版本有动画效果，IE低版本为静态效果"
+		  }
+			// usedTime : '设计3天&emsp;前端1.5天&emsp;详询设计和前端负责人'
+		},
+		{
+		  id: "1",
+		  title: "宜家多个页面切换+ip定位",
+		  landscape : false,
+		  platform: "PC,抽奖",
+		  sort: "专题案例",
+		  cover: "imgs/zt/2018/pc/cover3.png",
+		  images: [],
+		  demourl: "http://www1.pchouse.com.cn/zt/sh160408/ikea/cases_bed.html?id=01",
+		  apiurl: "",
+		  keywords: "ip定位，计算器插件，页面路径切换",
+		  desc: {
+		    intro: "ip定位，计算器插件，导航页面路径切换"
+		    // compatible: "pc端仅高版本有动画效果，IE低版本为静态效果"
+		  }
+			// usedTime : '设计3天&emsp;前端1.5天&emsp;详询设计和前端负责人'
+		},
+		{
+		  id: "1",
+		  title: "用canvas写字动效",
+		  landscape : false,
+		  platform: "H5",
+		  sort: "交互效果",
+		  cover: "imgs/zt/2018/canvas/cover.png",
+		  images: [],
+		  demourl: "http://www1.pchouse.com.cn/zt/sh20180321/mtmmh5/index.html",
+		  apiurl: "",
+		  keywords: "canvas动画",
+		  desc: {
+		    intro: "h5使用canvas写字动效"
+		    // compatible: "pc端仅高版本有动画效果，IE低版本为静态效果"
+		  }
+			// usedTime : '设计3天&emsp;前端1.5天&emsp;详询设计和前端负责人'
+		},
+		{
+		  id: "1",
+		  title: "横屏h5动画",
+		  landscape : false,
+		  platform: "H5",
+		  sort: "交互效果",
+		  cover: "imgs/zt/2018/h5/cover1.png",
+		  images: [],
+		  demourl: "http://www1.pconline.com.cn/zt/sh20161223/flphx/index.html",
+		  apiurl: "",
+		  keywords: "横屏h5动画,语音",
+		  desc: {
+		    intro: "横屏h5动画展示+点击语音对话播放"
+		    // compatible: "pc端仅高版本有动画效果，IE低版本为静态效果"
+		  }
+			// usedTime : '设计3天&emsp;前端1.5天&emsp;详询设计和前端负责人'
+		},
+		{
+		  id: "1",
+		  title: "模仿弹幕实现",
+		  landscape : false,
+		  platform: "H5",
+		  sort: "交互效果",
+		  cover: "imgs/zt/2018/pc/cover6.png",
+		  images: [],
+		  demourl: "",
+		  apiurl: "",
+		  keywords: "模仿弹幕实现",
+		  desc: {
+		    intro: "模仿弹幕实现"
+		    // compatible: "pc端仅高版本有动画效果，IE低版本为静态效果"
+		  }
+			// usedTime : '设计3天&emsp;前端1.5天&emsp;详询设计和前端负责人'
+		},
+		{
+		  id: "1",
+		  title: "pc晃动动效",
+		  landscape : false,
+		  platform: "pc",
+		  sort: "交互效果",
+		  cover: "imgs/zt/2018/pc/cover1.png",
+		  images: [],
+		  demourl: "http://www1.pclady.com.cn/zt/sh20170316/shangjing/index.html",
+		  apiurl: "",
+		  keywords: "pc晃动动效",
+		  desc: {
+		    intro: "pc晃动动效"
+		    // compatible: "pc端仅高版本有动画效果，IE低版本为静态效果"
+		  }
+			// usedTime : '设计3天&emsp;前端1.5天&emsp;详询设计和前端负责人'
+		},
+		{
+		  id: "1",
+		  title: "鼠标滚轮切换屏幕",
+		  landscape : false,
+		  platform: "pc",
+		  sort: "交互效果",
+		  cover: "imgs/zt/2018/pc/cover2.png",
+		  images: [],
+		  demourl: "http://www1.pconline.com.cn/zt/sh20161206/flp/index.html",
+		  apiurl: "",
+		  keywords: "鼠标滚轮",
+		  desc: {
+		    intro: "鼠标滚轮，分享，页面还有请求后台的搜索功能和根据接口返回数据设置翻页展示（接口已下线）"
+		    // compatible: "pc端仅高版本有动画效果，IE低版本为静态效果"
+		  }
+			// usedTime : '设计3天&emsp;前端1.5天&emsp;详询设计和前端负责人'
+		},
+		{
+		  id: "1",
+		  title: "摇一摇+H5动画呈现",
+		  landscape : false,
+		  platform: "h5",
+		  sort: "交互效果",
+		  cover: "imgs/zt/2018/h5/cover2.jpg",
+		  images: [],
+		  demourl: "http://www1.pchouse.com.cn/zt/sh20170413/yikou/index.html",
+		  apiurl: "",
+		  keywords: "摇一摇",
+		  desc: {
+		    intro: "摇一摇+h5动效组合展示"
+		    // compatible: "pc端仅高版本有动画效果，IE低版本为静态效果"
+		  }
+			// usedTime : '设计3天&emsp;前端1.5天&emsp;详询设计和前端负责人'
+		},
+	]
+}
+
